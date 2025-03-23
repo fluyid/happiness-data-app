100d39eb97dccd6042acf5b29f76b55ade6a2afc
Kailash Nath <kailashnathashok@gmail.com>
2025-03-23 22:31:38 +0300

    imported pandas, automated the dataframe creation process based on the value selected and plot the graph accordingly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import plotly.express as px
+import pandas as pd
 
 st.title("In Search for Happiness")
 
@@ -8,3 +9,16 @@ x_axis = st.selectbox("Select the data for the X-axis", options=options)
 y_axis = st.selectbox("Select the data for the Y-axis", options=options)
 
 st.subheader(f"{x_axis} and {y_axis}")
+
+happiness_score_df = pd.read_csv("happy.csv")
+
+x_value = happiness_score_df[f"{x_axis.lower()}"]
+y_value = happiness_score_df[f"{y_axis.lower()}"]
+
+labels = {
+    "x": x_axis,
+    "y": y_axis
+}
+
+figure = px.scatter(x=x_value, y=y_value, labels=labels)
+st.plotly_chart(figure)
